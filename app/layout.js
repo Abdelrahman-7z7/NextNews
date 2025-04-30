af43e2d3e9f0f2f74e4b3f22534e7f8e90b01f21
Abdelrahman Reyad <92989840+Abdelrahman-7z7@users.noreply.github.com>
2025-04-30 14:33:32 +0300

    creating the header of the app in the components to handle the navigation bar and add it to the layout to be visible in the whole routes

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css';
 
+import MainHeader from '@/components/main-header/main-header';
+
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
   description: 'Learn how to route to different pages.',
@@ -8,7 +10,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div id="page">
+          <MainHeader></MainHeader>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
