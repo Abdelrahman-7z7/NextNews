0c97dcb439a78a55b6ccbf853d4fed5aea79b2a4
Abdelrahman Reyad <92989840+Abdelrahman-7z7@users.noreply.github.com>
2025-04-30 14:37:33 +0300

    adjusting the app route landing page and the global style for the screen

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import Link from 'next/link'
+
 export default function HomePage() {
   return (
     <div id="home">
-      <h1>Next.js Routing & Page Rendering</h1>
+      <h1>Next News</h1>
     </div>
   );
 }
